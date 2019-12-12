dc4e792da45aa8022bcdc3b2872a59142bcfc022
Andrea Casalino <andrecasa91@gmail.com>
2019-12-12 23:04:08 +0100

    minor changes

--- content/src/Log_utils.h
+++ content/src/Log_utils.h
@@ -14,45 +14,6 @@
 #include "../src/Vector3d_basic.h"
 #include "FastQHull.h"
 
-void Log_CovexHull(const std::list<V>& cloud, const std::string& log_file, const size_t& Iterations  = 0) {
-
-	//build a new solver
-	Fast_QHull<V> convexHull_solver;
-
-	//compute the convex hull of the cloud
-	convexHull_solver.Compute_new_Convex_Hull(&cloud, Iterations);
-
-	std::ofstream f(log_file);
-	if (!f.is_open()) abort();
-
-	//log the cloud
-	for (auto it = cloud.begin(); it != cloud.end(); it++) 
-		f << it->x() << " " << it->y() << " " << it->z() << std::endl;
-
-	//add separator
-	f << "-1 -1 -1\n";
-
-	//log the incidences
-	size_t N_facets;
-	size_t* incidences = convexHull_solver.Get_Incidences(&N_facets);
-	for (size_t k = 0; k < N_facets; k++) 
-		f << incidences[3 * k] << " " << incidences[3 * k + 1] << " " << incidences[3 * k + 2] << std::endl;
-	//deallocate the incidences buffer
-	free(incidences);
-
-	//add separator
-	f << "-1 -1 -1\n";
-
-	//log the normals
-	std::list<V> normals;
-	convexHull_solver.Get_Normals(&normals);
-	for (auto it = normals.begin(); it != normals.end(); it++)
-		f << it->x() << " " << it->y() << " " << it->z() << std::endl;
-
-	f.close();
-
-}
-
 /** \brief Accepts a point clound and internally computes the convex hull.
 Then, the info describing the convex hull are saved in the file whose location
 is described by log_file. You can use Result_visualization/Render.html to visualize the results.
@@ -74,7 +35,7 @@ void Log_as_JSON_CovexHull(const std::list<V>& cloud, const std::string& log_fil
 	convexHull_solver.Get_Normals(&normals);
 
 
-	//put the above info in a textual file for displaying it, using Result_visualization/Render.html
+	//put the above info in a JSON file for displaying it, using Result_visualization/Render.html
 
 	std::ofstream f(log_file);
 	if (!f.is_open()) abort();
