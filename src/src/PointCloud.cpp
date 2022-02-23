3877251b01a4d79f059982183dc8c2fb3e529c1a
Andrea Casalino <andrea.casalino@cloudnc.com>
2022-02-23 16:43:26 +0000

    updating

--- src/src/PointCloud.cpp
+++ src/src/PointCloud.cpp
@@ -27,25 +27,60 @@ float get_vertex_distance(const hull::Coordinate &facet_point,
 std::unique_ptr<PointCloud::FarthestVertex>
 PointCloud::getFarthest(const hull::Coordinate &point_on_facet,
                         const hull::Coordinate &facet_normal) const {
-  bool found_at_least_one = false;
-  std::unique_ptr<PointCloud::FarthestVertex> result =
-      std::make_unique<PointCloud::FarthestVertex>(0, 0.f);
+  PointCloud::FarthestVertex result = PointCloud::FarthestVertex{0, 0.f};
+  float distance;
   for (std::size_t pos = 0; pos < points.size(); ++pos) {
     if (points_still_free[pos]) {
-      auto distance =
-          get_vertex_distance(point_on_facet, facet_normal, points[pos]);
+      distance = get_vertex_distance(point_on_facet, facet_normal, points[pos]);
       if ((QHULL_GEOMETRIC_TOLLERANCE < distance) &&
-          (result->distance < distance)) {
-        result->distance = distance;
-        result->vertex = pos;
-        found_at_least_one = true;
+          (result.distance < distance)) {
+        result.distance = distance;
+        result.vertex = pos;
       }
     }
   }
-  if (!found_at_least_one) {
+  if (result.distance <= QHULL_GEOMETRIC_TOLLERANCE) {
     return nullptr;
   }
-  return result;
+  return std::make_unique<PointCloud::FarthestVertex>(result);
 }
 
+namespace {
+template <typename DistanceComputation>
+std::size_t
+farthest_to_subject(const std::vector<hull::Coordinate> &points,
+                    const DistanceComputation &distance_to_subject) {
+  float max_distance = 0.f;
+  std::size_t result = 0;
+  float distance;
+  for (std::size_t k = 0; k < points.size(); ++k) {
+    distance = distance_to_subject(points[k]);
+    if ((distance > QHULL_GEOMETRIC_TOLLERANCE) && (distance > max_distance)) {
+      result = k;
+      max_distance = distance;
+    }
+  }
+  if (max_distance <= QHULL_GEOMETRIC_TOLLERANCE) {
+    throw Error{"The passed cloud has 0 volume"};
+  }
+}
+} // namespace
+
+std::vector<std::size_t> PointCloud::getInitialTethraedron() const {
+  std::vector<std::size_t> result;
+  result.reserve(4);
+
+  result.push_back(0);
+  const auto first_vertex = result.back();
+
+  result.push_back(farthest_to_subject(
+      points, [&subject = points[first_vertex]](const hull::Coordinate &point) {
+        return hull::squaredDistance(subject, point);
+      }));
+  const auto second_vertex = result.back();
+
+  // TODO
+
+  return result;
+}
 } // namespace qh
