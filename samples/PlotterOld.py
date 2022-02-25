b0c0a58ef80b94f28b03418fb887da64a66bd237
Andrea Casalino <andrea.casalino@cloudnc.com>
2022-02-25 15:16:49 +0000

    python script

---
+++ samples/PlotterOld.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+* Author:    Andrea Casalino
+* Created:   26.12.2019
+*
+* report any bug to andrecasa91@gmail.com.
+"""
+
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import matplotlib.pyplot as plt
+import json
+import sys
+
+
+
+def get_json_from_file(name):
+    with open(name) as json_file:
+        return json.load(json_file)
+    
+def plot_facet(Pa, Pb, Pc, ax, col, alp):
+    x = [Pa[0], Pb[0], Pc[0]]
+    y = [Pa[1], Pb[1], Pc[1]]
+    z = [Pa[2], Pb[2], Pc[2]]
+    ax.add_collection3d(Poly3DCollection([list(zip(x,y,z))], edgecolor='black', facecolors=col, alpha=alp, linewidth=0.2))
+
+def plot_CH(file, color, ax):
+    data = get_json_from_file(file)
+    
+    Vx = []
+    Vy = []
+    Vz = []
+    V = data['Cloud']
+    for i in V:
+        Vx.append(i[0])
+        Vy.append(i[1])
+        Vz.append(i[2])
+    ax.plot(Vx ,Vy ,Vz , '.r')
+    
+    for index in data['Index']:
+        plot_facet(data['Cloud'][index[0]], data['Cloud'][index[1]], data['Cloud'][index[2]], ax, color, 0.5)
+        
+    x_lim = [0,0]
+    y_lim = [0,0]
+    z_lim = [0,0]
+    for c in data['Cloud']:
+        if(c[0] < x_lim[0]): 
+            x_lim[0] = c[0]
+        if(c[0] > x_lim[1]): 
+            x_lim[1] = c[0]
+        
+        if(c[1] < y_lim[0]):
+            y_lim[0] = c[1]
+        if(c[1] > y_lim[1]):
+            y_lim[1] = c[1]
+
+        if(c[2] < z_lim[0]):
+            z_lim[0] = c[2]
+        if(c[2] > z_lim[1]):
+            z_lim[1] = c[2]
+            
+    lim = [min(x_lim[0],y_lim[0],z_lim[0]), max(x_lim[1],y_lim[1],z_lim[1])]        
+                        
+    ax.set_xlim3d(lim[0], lim[1])
+    ax.set_ylim3d(lim[0], lim[1])
+    ax.set_zlim3d(lim[0], lim[1])
+
+
+
+logName = sys.argv[1]
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+plot_CH(logName, 'green', ax)
+plt.show()
--- samples/utils/ImporterSTL.cpp
+++ samples/utils/ImporterSTL.cpp
@@ -11,19 +11,26 @@
 #include <fstream>
 #include <sstream>
 
+#include <math.h>
+#include <stdexcept>
+
 constexpr float TOLLERANCE_CLONE = static_cast<float>(1e-4);
+constexpr float TOLLERANCE_CLONE_SQUARED = TOLLERANCE_CLONE * TOLLERANCE_CLONE;
 
 std::vector<std::string> splitta_riga(const std::string &riga) {
   std::istringstream iss(riga);
   std::vector<std::string> slices;
-  while (true) {
-    if (iss.eof()) {
-      break;
-    }
-    slices.emplace_back();
-    iss >> slices.back();
+  while (!iss.eof()) {
+    iss >> slices.emplace_back();
   }
   return slices;
+}
+
+float squared_distance(const Vector3d &a, const Vector3d &b) {
+  float distance = std::pow(a.x() - b.x(), 2.f);
+  distance += std::pow(a.y() - b.y(), 2.f);
+  distance += std::pow(a.z() - b.z(), 2.f);
+  return distance;
 }
 
 std::vector<Vector3d> importSTL(const std::string &stlFileName) {
@@ -31,43 +38,32 @@ std::vector<Vector3d> importSTL(const std::string &stlFileName) {
 
   std::ifstream f(stlFileName);
   if (!f.is_open())
-    return {}; // invalid file
+    throw std::runtime_error{"Invalid stl file"};
 
-  std::string line;
-
-  std::size_t k;
-  std::getline(f, line);
-  float toll2 = TOLLERANCE_CLONE * TOLLERANCE_CLONE;
-  while (true) {
+  while (!f.eof()) {
+    std::string line;
     std::getline(f, line);
     auto slices = splitta_riga(line);
-    if (slices.front().compare("endsolid") == 0)
-      break;
+    if ((!slices.empty()) && (slices.front() == "vertex")) {
+      if (slices.size() != 4) {
+        throw std::runtime_error{"Invalid line inside stl file"};
+      }
+      Vector3d maybe_new_vertex =
+          Vector3d{static_cast<float>(atof(slices[1].c_str())),
+                   static_cast<float>(atof(slices[2].c_str())),
+                   static_cast<float>(atof(slices[3].c_str()))};
 
-    std::getline(f, line);
-    for (k = 0; k < 3; k++) {
-      std::getline(f, line);
-      slices = splitta_riga(line);
-      float x = static_cast<float>(atof(slices[1].c_str()));
-      float y = static_cast<float>(atof(slices[2].c_str()));
-      float z = static_cast<float>(atof(slices[3].c_str()));
-
-      auto it_vertices =
+      auto vertices_clone_it =
           std::find_if(imported_vertices.begin(), imported_vertices.end(),
-                       [&](const Vector3d &v) {
-                         float distance = (x - v.x()) * (y - v.x());
-                         distance += (y - v.y()) * (y - v.y());
-                         distance += (z - v.z()) * (z - v.z());
-                         return distance < toll2;
+                       [&maybe_new_vertex](const Vector3d &v) {
+                         return squared_distance(maybe_new_vertex, v) <=
+                                TOLLERANCE_CLONE_SQUARED;
                        });
-      if (it_vertices == imported_vertices.end()) {
-        imported_vertices.emplace_back(x, y, z);
+      if (vertices_clone_it == imported_vertices.end()) {
+        imported_vertices.push_back(maybe_new_vertex);
       }
     }
-    std::getline(f, line);
-    std::getline(f, line);
   }
-  f.close();
   return imported_vertices;
 }
 
